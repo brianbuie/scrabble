c295b8232c089d3441e65761a95e750c7727c5d8
Brian Buie <brianjamesbuie@gmail.com>
2022-07-16 22:15:54 -0500

    DIY https middleware

--- middleware.js
+++ middleware.js
@@ -1,3 +1,11 @@
-import sslRedirect from "next-ssl-redirect-middleware";
+import { NextResponse } from "next/server";
 
-export default sslRedirect({});
+const middleware = (req) => {
+  if (process.env.NODE_ENV === "production" && req.headers.get("x-forwarded-proto") !== "https") {
+    const hostname = req.headers.get("host") || req.nextUrl.hostname;
+    return NextResponse.redirect(`https://${hostname}${req.nextUrl.pathname}`, 301);
+  }
+  return NextResponse.next();
+};
+
+export default middleware;
